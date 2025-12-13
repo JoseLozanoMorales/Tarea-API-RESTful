30ffa6b9d7b432cf9921c53308162aa38de448b3
elkirita <jhinsoncelorio18@gmail.com>
2025-12-13 14:32:38 -0500

    AGREGADA LA PROGRAMACIÓN PARA MOSTRAR LOS LUGARES TURÍSTICOS CON UNA DE LAS 3 LIBRERÍAS QUE DABA LA ACTIVIDAD, EN ESTE CASO SE USÓ Volley

--- app/src/main/java/com/example/tareajsonapirestful/MainActivity.java
+++ app/src/main/java/com/example/tareajsonapirestful/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.tareajsonapirestful;
 
+
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +13,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private AutoCompleteTextView autocListaLugares;
+    private ArrayList<String> lugares;
+    private ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +38,58 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        String url = "https://turismo.quevedoenlinea.gob.ec/lugar_turistico/json_getlistadoGridLT";
+
+        autocListaLugares = findViewById(R.id.autocListaLugares);
+        lugares = new ArrayList<>();
+
+        adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_dropdown_item_1line,
+                lugares
+        );
+        autocListaLugares.setAdapter(adapter);
+
+        autocListaLugares.setThreshold(1);
+
+        CargarLugares(url);
+
+        autocListaLugares.setOnItemClickListener((parent, view, position, id) -> {
+            String lugar = parent.getItemAtPosition(position).toString();
+            Toast.makeText(this, lugar, Toast.LENGTH_SHORT).show();
+        });
+    }
+    private void CargarLugares(String url) {
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                response -> {
+                    try {
+                        JSONArray array = response.getJSONArray("data");
+
+                        for (int i = 0; i < array.length(); i++) {
+                            JSONObject obj = array.getJSONObject(i);
+
+                            String nombre = obj.getString("nombre_lugar");
+                            String categoria = obj.getString("categoria");
+
+                            String item = categoria + ": " + nombre;
+
+                            lugares.add(item);
+                        }
+
+                        adapter.notifyDataSetChanged();
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> Toast.makeText(this, "Error al intentar consumir la API", Toast.LENGTH_SHORT).show()
+        );
+
+        RequestQueue cola = Volley.newRequestQueue(this);
+        cola.add(request);
     }
 }
